1f1f92329adcddb4cce05f3b20c275e3883d52fd
rmania <dmeijerink1979@gmail.com>
2018-02-08 15:57:14 +0100

    cleaned up

--- extract/download_from_objectstore.py
+++ extract/download_from_objectstore.py
@@ -1,56 +1,36 @@
-"""
-Access the druktemonitor project data on the data store.
-
-For now the convention is that only relevant files are in the datastore and
-the layout there matches the layout expected by our data loading scripts.
-
-(We may have to complicate this in the future if we get to automatic
-delivery of new data for this project.)
-"""
+###############################################################################
+# functions to dowload data from the Cloud VPS Objectstore
+###############################################################################
 
 import os
 import argparse
 import logging
 import configparser
-
 from swiftclient.client import Connection
 
 logging.getLogger('requests').setLevel(logging.WARNING)
 logging.getLogger('urllib3').setLevel(logging.WARNING)
 logging.getLogger('swiftclient').setLevel(logging.WARNING)
-
 logger = logging.getLogger(__name__)
 
 FORMAT = '%(asctime)-15s %(message)s'
 logging.basicConfig(format=FORMAT, level=logging.DEBUG)
-
 config = configparser.RawConfigParser()
 config.read('auth.conf')
 
-#OBJECTSTORE_PASSWORD = os.environ['EXTERN_DATASERVICES_PASSWORD']
-OBJECTSTORE_PASSWORD = config.get('objectstore', 'OS_PASSWORD')
-
-OS_CONNECT = {
-    'VERSION': config.get('objectstore','ST_AUTH_VERSION'),
-    'AUTHURL': config.get('objectstore','OS_AUTH_URL'),
-    'TENANT_NAME': config.get('objectstore','OS_TENANT_NAME'),
-    'USER': config.get('objectstore','OS_USERNAME'),
-    'os_options': {
-        'TENANT_ID': config.get('objectstore','OS_PROJECT_ID'),  # Project ID
-        'REGION_NAME': config.get('objectstore','OS_REGION_NAME')
-    },
-    'PASSWORD': OBJECTSTORE_PASSWORD
-}
+# get config from auth.conf 
+OBJECTSTORE = dict(
+    VERSION = config.get('objectstore', 'VERSION'),
+    AUTHURL = config.get('objectstore', 'AUTHURL'),
+    TENANT_NAME = config.get('objectstore', 'TENANT_NAME'),
+    TENANT_ID = config.get('objectstore', 'TENANT_ID'),
+    USER = config.get('objectstore', 'USER'),
+    PASSWORD = config.get('objectstore', 'PASSWORD'),
+    REGION_NAME = config.get('objectstore', 'REGION_NAME')
+)
 
 
-DATASETS = set([
-    'Dataservices'
-])
 
-prefixes = ['aanvalsplan_schoon/mora']
-            #,'aanvalsplan_schoon/mora']
-
-    
 def get_connection(store_settings: dict) -> Connection:
     """
     get an objectsctore connection
@@ -79,19 +59,37 @@ def get_connection(store_settings: dict) -> Connection:
 
     return connection
 
-    
-    
-def get_full_container_list(conn, container, **kwargs) -> list:
-    """ 
-    List all available data sources in the specific data container specific to the os connection 
-    Returns a generator object.
+
+def get_object(connection, object_meta_data: dict, dirname: str):
     """
+    Download object from objectstore.
+    object_meta_data is an object returned when using 'get_full_container_list'
+    """
+    return connection.get_object(dirname, object_meta_data['name'])[1]
+
+def download_files(file_list):
+    """Download the latest data. """
+    for _, source_data_file in file_list:
+        sql_gz_name = source_data_file['name'].split('/')[-1]
+        msg = 'Downloading: %s' % (sql_gz_name)
+        log.debug(msg)
+
+        new_data = get_object(
+            mora_conn, source_data_file, 'Dataservices')
+
+        # save output to file!
+        with open('data/{}'.format(sql_gz_name), 'wb') as outputzip:
+            outputzip.write(new_data)
+            
+
+def get_full_container_list(conn, container, **kwargs) -> list:
     limit = 10000
     kwargs['limit'] = limit
     page = []
 
     _, page = conn.get_container(container, **kwargs)
     lastpage = page
+    
     for object_info in lastpage:
         yield object_info
 
@@ -104,62 +102,34 @@ def get_full_container_list(conn, container, **kwargs) -> list:
 
     raise StopIteration
     
-
-def download_container(conn, container, prefix, datadir):
-    
-    #target_dir = os.path.join(datadir, prefix)
-    #os.makedirs(target_dir)  # will error out if directory exists
-    
-    content = get_full_container_list(conn, container, prefix=prefix)
-    print(content)
-    for obj in content:
-        if obj['content_type']!='application/directory':
-            target_filename = os.path.join(datadir, obj['name'])
-            #target_filename = obj['name']
-            with open(target_filename, 'wb') as new_file:
-                _, obj_content = conn.get_object(container, obj['name'])
-                new_file.write(obj_content)
-        logger.debug('Written file '+obj['name'])
-
-
-def download_containers(conn, datasets, datadir):
+def get_latest_mora_files():
     """
-    Download the schoonmonitor datasets from object store.
-
-    Simplifying assumptions:
-    * layout on data store matches intended layout of local data directory
-    * datasets do not contain nested directories
-    * assumes we are running in a clean container (i.e. empty local data dir)
-    * do not overwrite / delete old data
+    Download the expected files provided by mks / kpn
     """
-    logger.debug('Checking local data directory exists and is empty')
-    if not os.path.exists(datadir):
-        raise Exception('Local data directory does not exist.')
+    file_list = []
 
-    listing = os.listdir(datadir)
-    if listing:
-        if len(listing) == 1 and listing[0] == 'README':
-            # Case where the 'data' dictory is used from a fresh checkout.
-            pass
-        else:
-            raise Exception('Local data directory not empty!')
+    meta_data = get_full_container_list(
+        mora_conn, 'Dataservices')
 
-    #logger.debug('Establishing object store connection.')
-    resp_headers, containers = conn.get_account()
+    for o_info in meta_data:
+        for expected_file in EXPECTED_FILES:
+            if not o_info['name'].endswith(expected_file):
+                continue
 
-    logger.debug('Downloading containers ...')
-    #dirName ='crow'
+            dt = parser.parse(o_info['last_modified'])
+            now = datetime.datetime.now()
 
-    #containers = conn.get_container('Dataservices',
-     #                            prefix='aanvalsplan_schoon/'+ dirName,)
-    #print(containers)
+            delta = now - dt
 
-    for c in containers:
-        if c['name'] in datasets:
-            for prefix in prefixes:
-                download_container(conn, c, prefix, datadir)
+            log.debug('AGE: %d %s', delta.days, expected_file)
 
 
+            log.debug('%s %s', EXPECTED_FILES, dt)
+            file_list.append((dt, o_info))
+
+    download_files(file_list)
+
+    
 def main(datadir):
     conn = Connection(**OS_CONNECT)
     download_containers(conn, DATASETS, datadir)
