5f3c0668d7274a86812f9570155a712e81f316b1
rmania <dmeijerink1979@gmail.com>
2018-02-08 17:39:10 +0100

    test

--- transform/enrichment/knmi.py
+++ transform/enrichment/knmi.py
@@ -8,100 +8,6 @@ import pandas as pd
 from io import StringIO
 from knmi_metadata import stations
 
-
-def get_day_data_raw(stations, start=None, end=None, inseason=False, variables=None):
-    """
-    Get daily weather data from KNMI
-    Parameters
-    ----------
-    stations : [int]
-        list of KNMI station numbers
-    start : datetime.datetime | str
-        date (optional, default is begin of current month)
-        can be a datetime object, or a string in format "%Y%m%d"
-    end : datetime.datetime | str
-        date (optional, default is today)
-        can be a datetime object, or a string in format "%Y%m%d"
-    inseason : bool (optional, default False)
-        see http://www.knmi.nl/kennis-en-datacentrum/achtergrond/data-ophalen-vanuit-een-script
-        for the full explanation
-    variables : list of variables to fetch (optional, default is ALL)
-        WIND = DDVEC:FG:FHX:FHX:FX wind
-        TEMP = TG:TN:TX:T10N temperatuur
-        SUNR = SQ:SP:Q Zonneschijnduur en globale straling
-        PRCP = DR:RH:EV24 neerslag en potentiële verdamping
-        PRES = PG:PGX:PGN druk op zeeniveau
-        VICL = VVN:VVX:NG zicht en bewolking
-        MSTR = UG:UX:UN luchtvochtigheid
-    Returns
-    -------
-    disclaimer, stations, legend, data
-    """
-
-    url = "http://projects.knmi.nl/klimatologie/daggegevens/getdata_dag.cgi"
-    params = {
-        "stns": ":".join(str(station) for station in stations),
-    }
-    if start is not None:
-        if not isinstance(start, str):
-            start = start.strftime("%Y%m%d")
-        params.update({"start": start})
-    if end is not None:
-        if not isinstance(start, str):
-            end = end.strftime("%Y%m%d")
-        params.update({"end": end})
-    if inseason is True:
-        params.update({"inseason": "Y"})
-    if variables is None:
-        variables = ['ALL']
-    params.update({"vars": ":".join(variables)})
-
-    r = requests.post(url=url, data=params)
-    if r.status_code != 200:
-        raise requests.HTTPError(r.status_code, url, params)
-
-    return parse_day_data(raw=r.text)
-
-
-def get_day_data_dataframe(stations, start=None, end=None, inseason=False, variables=None):
-    """
-    Get daily weather data from KNMI as a Pandas DataFrame
-    Parameters
-    ----------
-    stations : [int]
-        list of KNMI station numbers
-    start : datetime.datetime | str
-        date (optional, default is begin of current month)
-        can be a datetime object, or a string in format "%Y%m%d"
-    end : datetime.datetime | str
-        date (optional, default is today)
-        can be a datetime object, or a string in format "%Y%m%d"
-    inseason : bool (optional, default False)
-        see http://www.knmi.nl/kennis-en-datacentrum/achtergrond/data-ophalen-vanuit-een-script
-        for the full explanation
-    variables : list of variables to fetch (optional, default is ALL)
-        WIND = DDVEC:FG:FHX:FHX:FX wind
-        TEMP = TG:TN:TX:T10N temperatuur
-        SUNR = SQ:SP:Q Zonneschijnduur en globale straling
-        PRCP = DR:RH:EV24 neerslag en potentiële verdamping
-        PRES = PG:PGX:PGN druk op zeeniveau
-        VICL = VVN:VVX:NG zicht en bewolking
-        MSTR = UG:UX:UN luchtvochtigheid
-    Returns
-    -------
-    Pandas DataFrame
-    """
-
-    disclaimer, stations, legend, data = get_day_data_raw(stations=stations, start=start, end=end, inseason=inseason, variables=variables) 
-                                                          
-    df = parse_dataframe(data=data)
-    df.legend = legend
-    df.stations = stations
-    df.disclaimer = disclaimer
-
-    return df
-
-
 def parse_day_data(raw):
     """
     Parse the raw csv response of KNMI into relevant pieces.
@@ -199,4 +105,99 @@ def chunk_splitter(raw):
             chunk.append(line)
     else:
         yield chunk
+
+
+
+def get_day_data_raw(stations, start=None, end=None, inseason=False, variables=None):
+    """
+    Get daily weather data from KNMI
+    Parameters
+    ----------
+    stations : [int]
+        list of KNMI station numbers
+    start : datetime.datetime | str
+        date (optional, default is begin of current month)
+        can be a datetime object, or a string in format "%Y%m%d"
+    end : datetime.datetime | str
+        date (optional, default is today)
+        can be a datetime object, or a string in format "%Y%m%d"
+    inseason : bool (optional, default False)
+        see http://www.knmi.nl/kennis-en-datacentrum/achtergrond/data-ophalen-vanuit-een-script
+        for the full explanation
+    variables : list of variables to fetch (optional, default is ALL)
+        WIND = DDVEC:FG:FHX:FHX:FX wind
+        TEMP = TG:TN:TX:T10N temperatuur
+        SUNR = SQ:SP:Q Zonneschijnduur en globale straling
+        PRCP = DR:RH:EV24 neerslag en potentiële verdamping
+        PRES = PG:PGX:PGN druk op zeeniveau
+        VICL = VVN:VVX:NG zicht en bewolking
+        MSTR = UG:UX:UN luchtvochtigheid
+    Returns
+    -------
+    disclaimer, stations, legend, data
+    """
+
+    url = "http://projects.knmi.nl/klimatologie/daggegevens/getdata_dag.cgi"
+    params = {
+        "stns": ":".join(str(station) for station in stations),
+    }
+    if start is not None:
+        if not isinstance(start, str):
+            start = start.strftime("%Y%m%d")
+        params.update({"start": start})
+    if end is not None:
+        if not isinstance(start, str):
+            end = end.strftime("%Y%m%d")
+        params.update({"end": end})
+    if inseason is True:
+        params.update({"inseason": "Y"})
+    if variables is None:
+        variables = ['ALL']
+    params.update({"vars": ":".join(variables)})
+
+    r = requests.post(url=url, data=params)
+    if r.status_code != 200:
+        raise requests.HTTPError(r.status_code, url, params)
+
+    return parse_day_data(raw=r.text)
+
+
+def get_day_data_dataframe(stations, start=None, end=None, inseason=False, variables=None):
+    """
+    Get daily weather data from KNMI as a Pandas DataFrame
+    Parameters
+    ----------
+    stations : [int]
+        list of KNMI station numbers
+    start : datetime.datetime | str
+        date (optional, default is begin of current month)
+        can be a datetime object, or a string in format "%Y%m%d"
+    end : datetime.datetime | str
+        date (optional, default is today)
+        can be a datetime object, or a string in format "%Y%m%d"
+    inseason : bool (optional, default False)
+        see http://www.knmi.nl/kennis-en-datacentrum/achtergrond/data-ophalen-vanuit-een-script
+        for the full explanation
+    variables : list of variables to fetch (optional, default is ALL)
+        WIND = DDVEC:FG:FHX:FHX:FX wind
+        TEMP = TG:TN:TX:T10N temperatuur
+        SUNR = SQ:SP:Q Zonneschijnduur en globale straling
+        PRCP = DR:RH:EV24 neerslag en potentiële verdamping
+        PRES = PG:PGX:PGN druk op zeeniveau
+        VICL = VVN:VVX:NG zicht en bewolking
+        MSTR = UG:UX:UN luchtvochtigheid
+    Returns
+    -------
+    Pandas DataFrame
+    """
+
+    disclaimer, stations, legend, data = get_day_data_raw(stations=stations, start=start, end=end, inseason=inseason, variables=variables) 
+                                                          
+    df = parse_dataframe(data=data)
+    df.legend = legend
+    df.stations = stations
+    df.disclaimer = disclaimer
+
+    return df
+
 
