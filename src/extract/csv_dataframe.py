ae396f66c9bbef23a78957ce9fbcd31a1201deba
rmania <dmeijerink1979@gmail.com>
2018-02-12 17:10:48 +0100

    added argparse, main

--- src/extract/csv_dataframe.py
+++ src/extract/csv_dataframe.py
@@ -5,6 +5,7 @@
 
 import pandas as pd
 import numpy as np
+import argparse
 
 def read_crow_file(file, datecol):
     """
@@ -36,14 +37,6 @@ def read_crow_file(file, datecol):
 
 
 def read_mora_file(file, datecol):
-    """
-    parses the MORA Meldingen Openbare Ruimte Amsterdam
-    Args:
-        file (csv): containing at least a date column
-        datecol: in format %d-m-%Y %H:%M:%S
-    Returns:
-        * pd.DataFrame: cleaned data frame with datum and time column added
-    """
     
     df = (pd.read_csv(file, sep=';')
           .pipe(strip_cols)
@@ -67,6 +60,10 @@ def read_mora_file(file, datecol):
         
 def strip_cols(df):
     
+    """ 
+    simple utility function to clean dataframe columns
+    """
+    
     # clean columns names
     df.columns = df.columns.str.lower()
     df.columns = df.columns.str.replace(' ', '_')
@@ -77,3 +74,35 @@ def strip_cols(df):
     
     df = df.copy()
     return df
+
+
+def parser():
+    """Parser function to run arguments from the command line and to add description to sphinx."""
+    parser = argparse.ArgumentParser(description=
+    """ parses the MORA Meldingen Openbare Ruimte Amsterdam once downloaded from objectstore.
+    Args:
+        file (csv): containing at least a date column
+        datecol: in format %d-m-%Y %H:%M:%S
+    Returns:
+        * pd.DataFrame: cleaned data frame with datum and time column added
+    command line example: 
+      `read_mora_file(PATH_TO_MORA_FILE + MORA_FILE, datecol='aa_adwh_datum_melding')`
+    """
+                                    )
+    parser.add_argument('file',
+                        type=str,
+                        help="MORA file to be loaded in")
+    parser.add_argument('datecol',
+                        type=date,
+                        help="date column from which the date and time are extracted and put into different \
+                        columns. The mora date column in source file is 'aa_adwh_datum_melding' ")
+    return parser
+
+
+def main():
+    args = parser().parse_args()
+    print(args)
+    read_mora_file(args.file, args.datecol)
+
+if __name__ == '__main__':
+    main()
