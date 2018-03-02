5c30c4c85b9c256372ccb0bc231cbb22b5f01118
rmania <dmeijerink1979@gmail.com>
2018-03-02 11:58:03 +0100

    os connection w logger info added get config

--- src/helpers/connections.py
+++ src/helpers/connections.py
@@ -11,6 +11,7 @@ logging.basicConfig(level=logging.INFO,
                     format='%(asctime)s %(levelname)-8s %(message)s',
                     datefmt='%a, %d %b %Y %H:%M:%S')
 logger = logging.getLogger(__name__)
+logger = logging.getLogger('objectstore')
 
 # -----------------
 # Database stuff
@@ -40,17 +41,37 @@ def postgres_engine_pandas(config_full_path, db_config_name):
 # -----------------
 # Objectstore
 # -----------------
-
-def objectstore_connection(config_full_path, config_name):
-    """Get objectstore settings as a dict
-       :config_full_path = /path_to_config/config.ini or config.ini if in root.
-       :config_name = objectstore
+def get_config(full_path):
     """
+    Get config file with login credentials, port numbers...
+    Args:
+        full_path - provide path tot he config file/ auth.conf etc.
+     """
+    config = configparser.RawConfigParser()
+    config.read(full_path)
+    logger.info('Found these configs.. {}'.format(config.sections()))
+     
+    return config
+
+
+def objectstore_connection(config_full_path, config_name, print_config_vars=None):
+    """
+      get an objectsctore connection
+      args:
+          config_full_path = /path_to_config/config.ini or config.ini if in root.
+          config_name = objectstore
+          print_config_vars: if set to True: print all variables from the config file
+      returns:
+          objectstore connection
+      """
+    
     assert os.environ['OBJECTSTORE_PASSWORD']
 
-    config = configparser.RawConfigParser()
-    logger.info('Connecting to config..: {}'.format(config_name))
-    config.read(config_full_path)
+    config = get_config(config_full_path)
+        
+    if print_config_vars:
+         logger.info('config variables.. :{}'.format(OBJECTSTORE))
+    
     conn = Connection(authurl=config.get(config_name, 'AUTHURL'),
                       user=config.get(config_name, 'USER'),
                       key=os.environ['OBJECTSTORE_PASSWORD'],
@@ -60,5 +81,7 @@ def objectstore_connection(config_full_path, config_name):
                                   'region_name': config.get(config_name, 'REGION_NAME'),
                                   # 'endpoint_type': 'internalURL'
                                   })
+    
     logger.info('Established successfull connection to {}'.format(config.get(config_name, 'TENANT_NAME')))
+    
     return conn
